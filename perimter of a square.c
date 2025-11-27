2586f28251dc1d6e68023f203f39f808ecfc93df
Vamsikrishna329-in <krishnavamsi62365@gmail.com>
2025-11-27 18:41:26 +0530

    Perimeter of a square

---
+++ perimter of a square.c
@@ -0,0 +1,10 @@
+//perimter of a square
+#include<stdio.h>
+int main(){
+    double n,peri;
+    printf("Enter the side of a Square:");
+    scanf("%lf",&n);
+    peri = 4*n;
+    printf("The area of the Square if side %.2lf is: %.3lf",n,peri);
+    return 0;
+}
