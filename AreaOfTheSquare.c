c56c9e3846875dad9195680738f7c7370bf0ee9a
Vamsikrishna329-in <krishnavamsi62365@gmail.com>
2025-11-27 18:32:40 +0530

    Area of a square

--- AreaOfTheSquare.c
+++ AreaOfTheSquare.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 int main(){
     double n,area;
-    printf("Enter the side od a Square:");
+    printf("Enter the side of a Square:");
     scanf("%lf",&n);
     area = n*n;
     printf("The area of the Square if side %.2lf is: %.3lf",n,area);
