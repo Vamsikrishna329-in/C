059179674ab6781f0fc606d57818856e61041dfb
Vamsikrishna329-in <krishnavamsi62365@gmail.com>
2025-11-27 22:26:37 +0530

    max number finder in an array

---
+++ maxNumberInArray.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the size of array:");
+    scanf("%d",&n);
+    int arr[n];
+    
+    printf("Enter the elements:\n");
+    for(int i = 0; i < n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int temp = arr[0];
+    int size = sizeof(arr)/sizeof(arr[0]);
+    for(int i = 0; i < n; i++){
+        if(temp < arr[i]){
+        temp = arr[i];
+        }
+    }
+    printf("Maximum number is : %d",temp);
+    return 0;
+
+}
