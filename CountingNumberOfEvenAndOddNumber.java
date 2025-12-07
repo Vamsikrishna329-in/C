d16a68b8fd02026820ac38cb89ef14fa266080d1
Vamsikrishna329-in <krishnavamsi62365@gmail.com>
2025-12-07 15:15:15 +0530

    CountingNumberOfEvenAndOddNumber.java

---
+++ CountingNumberOfEvenAndOddNumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        int even = 0; 
+        int odd = 0;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the size:");
+        int n = scan.nextInt();
+        int[] values = new int[n];
+        System.out.print("Enter the values:");
+        for(int i = 0;i < n;i++){
+            values[i] = scan.nextInt();
+            if(values[i] % 2 == 0){
+                even ++;
+            }else{
+                odd ++;
+            }
+        }
+        System.out.println("The Number Of Even numbers are: "+ even+ "\n The Number Of Odd number are:"+odd);
+        
+    }
+}
