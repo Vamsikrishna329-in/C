9aaa22935aa31a700a79e892b1c980d558879747
Vamsikrishna329-in <krishnavamsi62365@gmail.com>
2025-12-16 21:33:30 +0530

    GreatestOfThreeNumbers.java
    Finding of greatest of three numbers using Ternary operator

---
+++ GreatestOfThreeNumbers.java
@@ -0,0 +1,6 @@
+class Main {
+    public static void main(String[] args) {
+        int n1 = 11,n2 = 22,n3 = 33;
+        System.out.println("The Greatest Value: "+((n1>n2)&& (n1>n3)?n1:((n2>n3)?n2:n3)));
+    }
+}
